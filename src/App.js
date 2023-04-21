7ea946fb15eb00b4dfe173ba402c2459724b73e2
nehasharma0905 <sharma.110@iitj.ac.in>
2023-04-22 04:39:43 +0530

    Expense-Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+// one input box, one todo list box
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [price, setPrice] = useState([]);
+  const [p, setP] = useState();
+  const [i, setI] = useState("");
+  const [t, setT] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="Todo">
+      <h2 class="heading">Expense Calculator</h2>
+      <div className="input_1">
+        <h3 class="abc">Items:</h3>
+        <input
+          class="input_box"
+          value={i}
+          className="items"
+          onChange={(e1) => setI(e1.target.value)}
+        />
+        <h3 class="abc">Price:</h3>
+        <input
+          class="input_box"
+          value={p}
+          className="price"
+          onChange={(e2) => setP(e2.target.value)}
+        />
+        <button
+          class="button"
+          onClick={() => {
+            setItems([...items, i]);
+            setPrice([...price, p]);
+            setI("");
+            setP("");
+            setT(t + parseFloat(p));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
+
+      <div className="list_box">
+        <ul class="list">
+          {items.map((el, index) => (
+            <li>{el} :</li>
+          ))}
+        </ul>
+        <ul class="list">
+          {price.map((el, index) => (
+            <li>$ {el}</li>
+          ))}
+        </ul>
+      </div>
+      <h2 class="total">Total Expenses: ${t}</h2>
     </div>
   );
 }
